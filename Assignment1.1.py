b842b045fe3b16a2257655ca4931a6d541549be8
jilles <jilles@ziggo.nl>
2021-11-24 16:47:14 +0100

    1b gedaan, bezig met 1c

---
+++ Assignment1.1.py
@@ -0,0 +1,71 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+random.seed(1)
+#E-vtol assignment
+
+X = [1, 2, 3, 4, 5, 6]
+prob_dist = [0.01, 0.2, 0.5, 0.25, 0.03, 0.01]
+t_start = 9 * 60
+t_end = 11 * 60
+sep = 3
+vtols = 12
+
+#check amount of simulation runs
+numb_of_runs = 0
+i = []
+average = []
+runs = []
+
+# while numb_of_runs <= 5000:
+#     dist = [0, 0, 0, 0, 0, 0]
+#     while sum(dist) < vtols:
+#         x = np.random.choice(X, p = prob_dist)
+#         dist[X.index(x)] += 1
+#
+#     #check for x = 2, mean should be 6
+#     i.append(dist[2])
+#     average.append(np.average(i))
+#     runs.append(numb_of_runs)
+#     numb_of_runs += 1
+#
+# plt.plot(runs, average)
+# plt.plot(runs, 6* np.ones(len(runs)))
+# plt.xlabel('Number of simulation runs')
+# plt.ylabel('Average number of e-VTOLs at i = 3')
+# plt.show()
+
+#a.
+
+# while numb_of_runs <= 2000:
+#     dist = [0, 0, 0, 0, 0, 0]
+#     while sum(dist) < vtols:
+#         x = np.random.choice(X, p = prob_dist)
+#         dist[X.index(x)] += 1
+#
+#     #check for x = 2, mean should be 6
+#     i.append(dist[1])
+#     average.append(np.average(i))
+#     runs.append(numb_of_runs)
+#     numb_of_runs += 1
+#
+# print(np.average(average))
+
+#b.
+T_delay = []
+while numb_of_runs <= 2000:
+    numb_of_runs += 1
+    t_delay = 0
+    arrival_time_dist = []
+    for i in range(12):
+        arrival_time = random.randrange(t_start, t_end)
+        arrival_time_dist.append(arrival_time)
+
+    for i in range(len(arrival_time_dist)-1):
+        if abs(arrival_time_dist[i] - arrival_time_dist[i+1]) < sep:
+            t_delay += sep - abs(arrival_time_dist[i] - arrival_time_dist[i+1])
+    T_delay.append(t_delay)
+
+print(np.average(T_delay))
