ad935afa5482298f4961cbd8f6a76eaadcac08b9
jilles <jilles@ziggo.nl>
2021-11-29 18:23:59 +0100

    Progress 1.2

--- Assignment1/Assignment1.2.py
+++ Assignment1/Assignment1.2.py
@@ -1,15 +1,37 @@
 import numpy as np
 import pandas as pd
 from scipy.special import gamma
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv(open('data.csv', 'r'))
-brakes = ['Brake-1', 'Brake-2', 'Brake-3', 'Brake-4', 'Brake-5', 'Brake-6', 'Brake-7', 'Brake-8']
 
 gamma_func = []
 
-print(gamma(list(data['Brake-1'])))
+
 
 # for brake in brakes:
 #     gamma_func.append(gamma(data[brake]))
 #     print(gamma_func)
 
+with open('data.csv', 'r') as f:
+    df = pd.read_csv(f, sep=',', header=0)
+    data = df.values
+
+
+v_brakes = np.zeros((299,8))
+
+for i in range(1,9):
+    degradation = data[:,i]
+    v_brake = []
+    for j in range(len(degradation)-1):
+        v = degradation[j+1] - degradation[j]
+
+        if v <= -0.95:
+            v = 0
+
+        v_brake.append(v)
+    v_brakes[:,i-1] = v_brake
+
+print(v_brakes)
+sns.histplot(v_brakes)
+plt.show()
