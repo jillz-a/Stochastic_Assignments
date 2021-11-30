4ad30d36727dc1201e91be3d3965bfffe09ffa95
jilles <jilles@ziggo.nl>
2021-11-30 09:59:53 +0100

    terug bij het oude

--- Assignment1/Assignment1.2.py
+++ Assignment1/Assignment1.2.py
@@ -17,7 +17,8 @@ with open('data.csv', 'r') as f:
     df = pd.read_csv(f, sep=',', header=0)
     data = df.values
 
-
+n = 8
+alpha = np.linspace(0.001, 1, 999)
 v_brakes = np.zeros((299,8))
 
 for i in range(1,9):
@@ -33,8 +34,11 @@ for i in range(1,9):
     v_brakes[:,i-1] = v_brake
 
 
-v_brakes = np.concatenate(v_brakes)
-v_brakes = v_brakes[v_brakes != 0.0]
-sns.histplot(v_brakes)
+v_total = np.concatenate(v_brakes)
+v_total = v_total[v_total != 0.0]
+# sns.histplot(v_total)
+
+likelihood = n*(alpha - 1)*sum([np.log(y) for y in v_total])/n - n*alpha - n*alpha*np.log(gamma(alpha)) - n*alpha*np.log(sum(v_total)/n) + n*alpha*np.log(alpha)
+plt.plot(alpha, likelihood)
 
 plt.show()
