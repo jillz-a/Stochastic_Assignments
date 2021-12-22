ee860d9a9d8548f2e22a67e2d63e3fa04f42d45f
jilles <jilles@ziggo.nl>
2021-12-22 13:35:17 +0100

    begonnen aan 2.1f

--- Assignment2/Assignment2.1.py
+++ Assignment2/Assignment2.1.py
@@ -8,44 +8,47 @@ Code written by Jilles Andringa & Mitchell de Keijzer
 '''
 iterations = 0
 
-avg_wait_time = []
+buisness = True #True if there is a buisness class line, False if not
 wait_time = []  # time a passenger needs to wait
+avg_wait_time = [] # average time a passenger needs to wait
 
 #Monte Carlo simulation
 while iterations < 2000:
     iterations += 1
 
     #setup intial values
-    pax_que = [] #passenger que, contains arrival times if all desks are full
+    time = 0  # starting time
+    pax_que = [] #passenger que, contains passenger arrival times
+    bsns_que = [] #buisness class passenger que, contains passenger arrival times
     desks = [0,0,0] #available desks, 0 if available, pax_arrival if not
     pax_arrival = 0 #passenger arrival time
-    time = 0 #starting time
 
 
     while time <= 180: #set the time interval in minutes
         pax_arrival += np.random.exponential(1/2.5) #calculate the sojourn time for when next passenger arrives
         pax_que.append(pax_arrival)
 
+        #Variable time step, check what happens first, desk becomes available or new passenger arrives
         if any(desk != 0 for desk in desks):
-            first_desk = min(desk for desk in desks if desk != 0)
+            first_desk = min(desk for desk in desks if desk != 0) #first desk that will become available
             time = min(pax_arrival, first_desk)
         else:
             time = pax_arrival
 
-        for i in range(len(desks)): #check if desk has become available
+        # check if desk has become available
+        for i in range(len(desks)):
             if pax_arrival >= desks[i]:
                 desks[i] = 0 #reset desk to available
 
-        if any(desk == 0 for desk in desks) and len(pax_que) != 0: #check for available desks
+        # check for available desks
+        if any(desk == 0 for desk in desks) and len(pax_que) != 0:
             for i in range(len(desks)):
                 if desks[i] == 0 and len(pax_que) != 0: #assign passenger to an available desk
-                    # print('Desk', i, 'is available')
                     desks[i] = time + np.random.exponential(1) #time when desk will become available again
                     pax_que.remove(pax_que[0]) #passenger is removed from the que
 
-
-        elif all(desk != 0 for desk in desks) and len(pax_que) != 0: #if all desk are full, note the waiting time
-            # print('All desks are full')
+        # if all desk are full, note the waiting time
+        elif all(desk != 0 for desk in desks) and len(pax_que) != 0:
             wait_time.append(abs(min(desks) - pax_que[0]))
 
     if len(wait_time) != 0:
@@ -53,4 +56,10 @@ while iterations < 2000:
 
 
 plt.plot(avg_wait_time)
+plt.ylabel('Waiting time in minutes')
+plt.xlabel('Iterations')
+plt.show()
+
+sns.histplot(wait_time)
+plt.xlabel('Waiting time in minutes')
 plt.show()
