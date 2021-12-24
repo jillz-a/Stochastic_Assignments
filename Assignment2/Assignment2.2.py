ece074bb341bf65eb895095e0850c64016f16441
Mitchell <mitchelldekeijzer@gmail.com>
2021-12-24 13:29:18 +0100

    2.2 af & 2.3 bijna af

--- Assignment2/Assignment2.2.py
+++ Assignment2/Assignment2.2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics
 
 '''
 Assignment 2.2 AE4426-19 Stochastic Processes and Simulation
@@ -28,12 +29,24 @@ average_time = []
 Yt_list = [] # lists all Y_t values at t=30
 Exp_Yt = [] # gives the average of all current Y_t values in Yt_list
 
+X_1 = []
+X_2 = []
+X_avg_1 = []
+X_avg_2 = []
+X = []
+
 # Monte carlo simulation
-while iterations < 2000:
+while iterations < 8000:
     V_t = np.array([[np.random.normal(0, 1)], [np.random.normal(0, 1)]])
     e_t = np.random.normal(0, 0.1)
     Y_t = np.matmul(C, X_t) + e_t
-    X_t = np.matmul(A, X_t) + B + V_t
+    X_t = np.matmul(A, X_t) + B + V_t # X_{t+1}
+    # if t == 2: # X_{t+1} = X_3
+    #     X_1.append(X_t[0][0])
+    #     X_2.append(X_t[1][0])
+    #     X_avg_1.append(np.mean(X_1))
+    #     X_avg_2.append(np.mean(X_2))
+    #     X.append(X_t)
     if t == 30:
         Yt_list.append(Y_t)
         Exp_Yt.append(np.mean(Yt_list))
@@ -43,16 +56,31 @@ while iterations < 2000:
         runs.append(iterations)
         iterations += 1
         X_t = np.array([[0], [0]])
-        t = -1
+        t = 0
     t += 1
-print('L=1000 exceeded at time', np.mean(times))
+
+# plt.plot(runs, X_avg_1)
+# plt.plot(runs, X_avg_2)
+# plt.xlabel("Simulation runs")
+# plt.ylabel("X")
+# plt.show()
+
+mean_times = np.mean(times)
+var_times = np.var(times)
+CI = (mean_times - 1.96 * np.sqrt(var_times / 8000), mean_times + 1.96 * np.sqrt(var_times / 5000))
+print('L=1000 exceeded at minimum time', mean_times)
+print('Confidence interval:', CI)
 plt.plot(runs, average_time)
 plt.xlabel("Simulation runs")
 plt.ylabel("Time")
 plt.show()
 
 # 2c
-print('Expected level of degradation at t=30:', np.mean(Yt_list))
+mean_Yt = np.mean(Yt_list)
+var_Yt = np.var(Yt_list)
+CI = (mean_Yt - 1.96 * np.sqrt(var_Yt / 8000), mean_Yt + 1.96 * np.sqrt(var_Yt / 5000))
+print('Expected level of degradation at t=30:', mean_Yt)
+print('Confidence interval', CI)
 plt.plot(runs, Exp_Yt)
 plt.xlabel("Simulation runs")
 plt.ylabel("Measured level of degradation")
--- Assignment2/Assignment2.3.py
+++ Assignment2/Assignment2.3.py
@@ -1,45 +1,83 @@
 import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+'''
+Assignment 2.3 AE4426-19 Stochastic Processes and Simulation
+Code written by Jilles Andringa & Mitchell de Keijzer
+
+Variables:
+X_t: The degradation over time of a component (wearing)
+t: time
+B_t: standard Brownian motion
+
+Constants:
+beta: degradation drift
+sigma: diffusion coefficient
+
+'''
+
+np.random.seed(5)
+
+# constants
 beta = 3
 sigma = 5
-dt = 0.1
-t = 0
-x_list = []
-var_list = []
-Exp = []
-runs = []
-var = []
-iterations = 0
-x = 0
 
-# Bt = np.random.normal(0, 3)
-# print(Bt)
-# Xt = beta * 3 + sigma * Bt
+# starting values
+t = 3 # as asked for in the assignment
+iterations = 0
+X_t = 0 # we are considering X_0 = 0
+
+# lists
+Xt2_list = [] # list for the (X_3)^2 values
+Xt_list = [] # list for the X_3 values
+Exp = [] # list for the mean values of the (X_3)^2 values
+Var = [] # list for the variance values of the X_3 values
+runs = [] # list for the amount of simulation runs
+
 
 while iterations < 10000:
-    Bt = np.random.normal(0, t)
-    # Bt = norm.pdf(x)
-    x = (beta * t + sigma * Bt)
-    # x_list.append(x**2)
-    # Exp.append(np.mean(x_list))
+    Bt = np.random.normal(0, np.sqrt(3)) # sigma^2 = t - s
+    X_t = (beta * t + sigma * Bt)
+    Xt2_list.append(X_t ** 2)
+    Xt_list.append(X_t)
+    Exp.append(np.mean(Xt2_list))
+    Var.append(np.var(Xt_list))
+    runs.append(iterations)
+    if iterations == 10:
+        plt.plot(runs, Var)
+        plt.xlabel('Simulation run')
+        plt.ylabel('Variance of X_t')
+        plt.show()
+    if iterations == 100:
+        plt.plot(runs, Var)
+        plt.xlabel('Simulation run')
+        plt.ylabel('Variance of X_t')
+        plt.show()
+    iterations += 1
 
-    if t == 3:
-        runs.append(iterations)
-        iterations += 1
-        x_list.append(x**2)
-        var_list.append(x)
-        Exp.append(np.mean(x_list))
-        var.append(np.var(var_list))
-        x = 0
-        t = 0
-    t += 1
-
-    # print(x)
-
+print('E[(X_3)^2] = ', np.mean(Xt2_list))
+print('Var(X_3) = ', np.var(Xt_list))
 plt.plot(runs, Exp)
-plt.plot(runs, var)
+plt.xlabel('Simulation run')
+plt.ylabel('Average (X_t)^2 value')
+plt.show()
+plt.xlabel('Simulation run')
+plt.ylabel('Variance of X_t')
+plt.plot(runs, Var)
+plt.show()
+
+# plt.hist(Xt_list)
+sns.histplot(data=Xt_list, binwidth=1)
+plt.show()
+
+dirac = []
+for i in range(len(Xt_list)):
+    px = 1 / (i + 1) * Xt_list[i]
+    dirac.append(px)
+
+y = np.ones(len(Xt_list))
+plt.stem(Xt_list, y)
 plt.show()
 
 
