1366de65715ec0e07d1e5480adffe51bef8f5f8e
Mitchell <mitchelldekeijzer@gmail.com>
2022-01-04 21:17:42 +0100

    2.2 af & 2.3 bijna af

--- Assignment2/Assignment2.3.py
+++ Assignment2/Assignment2.3.py
@@ -17,7 +17,7 @@ sigma: diffusion coefficient
 
 '''
 
-np.random.seed(5)
+np.random.seed(1)
 
 # constants
 beta = 3
@@ -25,6 +25,7 @@ sigma = 5
 
 # starting values
 t = 3 # as asked for in the assignment
+SD = np.sqrt(3) # sigma^2 = t - s --> sigma = sqrt(3) for np.random.normal(mu, sigma)
 iterations = 0
 X_t = 0 # we are considering X_0 = 0
 
@@ -37,7 +38,7 @@ runs = [] # list for the amount of simulation runs
 
 
 while iterations < 10000:
-    Bt = np.random.normal(0, np.sqrt(3)) # sigma^2 = t - s
+    Bt = np.random.normal(0, SD)
     X_t = (beta * t + sigma * Bt)
     Xt2_list.append(X_t ** 2)
     Xt_list.append(X_t)
@@ -45,30 +46,54 @@ while iterations < 10000:
     Var.append(np.var(Xt_list))
     runs.append(iterations)
     if iterations == 10:
+        print('E[(X_3)^2] at 10 runs:', np.mean(Xt2_list))
+        print('Var(X_3) at 10 runs:', np.var(Xt_list))
+        plt.subplot(3, 2, 1)
         plt.plot(runs, Var)
-        plt.xlabel('Simulation run')
-        plt.ylabel('Variance of X_t')
-        plt.show()
+        plt.title("Variance")
+        plt.subplot(3, 2, 2)
+        plt.plot(runs, Exp)
+        plt.title("Expectation")
+        # plt.xlabel('Simulation run')
+        # plt.ylabel('Variance of X_t')
+        # plt.show()
     if iterations == 100:
+        print('E[(X_3)^2] at 100 runs:', np.mean(Xt2_list))
+        print('Var(X_3) at 100 runs:', np.var(Xt_list))
+        plt.subplot(3, 2, 3)
         plt.plot(runs, Var)
-        plt.xlabel('Simulation run')
-        plt.ylabel('Variance of X_t')
-        plt.show()
+        plt.ylabel('Variance of X-value for t = 3 [-]')
+        plt.subplot(3, 2, 4)
+        plt.plot(runs, Exp)
+        plt.ylabel('Expectation of the squared X-value for t = 3 [-]')
+        # plt.xlabel('Simulation run')
+
+        # plt.show()
     iterations += 1
 
-print('E[(X_3)^2] = ', np.mean(Xt2_list))
-print('Var(X_3) = ', np.var(Xt_list))
-plt.plot(runs, Exp)
-plt.xlabel('Simulation run')
-plt.ylabel('Average (X_t)^2 value')
-plt.show()
-plt.xlabel('Simulation run')
-plt.ylabel('Variance of X_t')
+print('E[(X_3)^2] at 10000 runs = ', np.mean(Xt2_list))
+CI = (np.mean(Xt2_list) - 1.96 * np.sqrt(np.var(Xt2_list) / 10000), np.mean(Xt2_list) + 1.96 * np.sqrt(np.var(Xt2_list) / 10000))
+print('CI', CI)
+print('Var(X_3) at 10000 runs = ', np.var(Xt_list))
+
+plt.subplot(3, 2, 5)
+plt.xlabel('Simulation run [-]')
+# plt.ylabel('Variance of X_t')
 plt.plot(runs, Var)
+plt.subplot(3, 2, 6)
+plt.xlabel('Simulation run [-]')
+plt.plot(runs, Exp)
 plt.show()
+
+# plt.plot(runs, Exp)
+# plt.xlabel('Simulation run')
+# plt.ylabel('Average (X_t)^2 value')
+# plt.show()
+
 
 # plt.hist(Xt_list)
 sns.histplot(data=Xt_list, binwidth=1)
+plt.xlabel('X-value [-]')
 plt.show()
 
 dirac = []
@@ -77,7 +102,7 @@ for i in range(len(Xt_list)):
     dirac.append(px)
 
 y = np.ones(len(Xt_list))
-plt.stem(Xt_list, y)
+plt.stem(dirac, y)
 plt.show()
 
 
